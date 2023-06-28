94438cd5dcfccf0eb6a8d33844c2b70e10e2ddc3
Gena-Sobhy <124585283+Gena-Sobhy@users.noreply.github.com>
2023-06-28 22:25:14 +0300

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import GetStarted from "./GetStarted";
+import Stats from "./stats";
+import Business from "./Business";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Testimonials from "./Testimonials";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+
+export {
+    Navbar ,
+    Hero ,
+    GetStarted ,
+    Stats ,
+    Business ,
+    Billing ,
+    CardDeal ,
+    Testimonials ,
+    Clients ,
+    CTA ,
+    Footer
+}
